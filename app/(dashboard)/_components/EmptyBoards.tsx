34a1c7a728b4b3347c05b2e8b2e163cde6180365
Supriyo Saha <mm.saha1982@gmail.com>
2024-02-05 21:27:57 +0530

    redirect fix

--- app/(dashboard)/_components/EmptyBoards.tsx
+++ app/(dashboard)/_components/EmptyBoards.tsx
@@ -5,11 +5,12 @@ import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { useOrganization } from "@clerk/nextjs";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 export const EmptyBoards = () => {
   const { organization } = useOrganization();
-
+const router = useRouter()
   const { mutate, pending } = useApiMutation(api.board.create);
 
   const onClick = () => {
@@ -21,7 +22,7 @@ export const EmptyBoards = () => {
     })
       .then((id) => {
         toast.success("Board created!");
-        //redirect to board/id
+        router.push(`/board/${id}`)
       })
       .catch(() => toast.error("Failed to create board!"));
   };
