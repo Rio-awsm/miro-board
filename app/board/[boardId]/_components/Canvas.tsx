176a94e71f46bdb7d09a1ef7e615e879eb18e550
Supriyo Saha <mm.saha1982@gmail.com>
2024-02-08 15:16:00 +0530

    canvas state

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -1,25 +1,48 @@
-"use client"
+"use client";
 
-import { useSelf } from "@/liveblocks.config"
-import Info from "./Info"
-import Toolbar from "./Toolbar"
-import Participants from "./participants"
-
+import { useCanRedo, useCanUndo, useHistory, useSelf } from "@/liveblocks.config";
+import Info from "./Info";
+import Toolbar from "./Toolbar";
+import Participants from "./participants";
+import { useState } from "react";
+import {
+  Camera,
+  CanvasMode,
+  CanvasState,
+  Color,
+  LayerType,
+  Point,
+  Side,
+  XYWH,
+} from "@/types/canvas";
 
 interface CanvasProps {
   boardId: string;
-};
+}
 
-const Canvas = ({boardId} : CanvasProps) => {
+const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
 
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
 
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
-  )
-}
+  );
+};
 
-export default Canvas
+export default Canvas;
